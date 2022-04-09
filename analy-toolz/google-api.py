cb129b0be1eb6cd9191794a01b4326df0fdb76fb
Makoto Shimizu <mak@fish.razor.jp>
2022-04-09 11:55:48 +0900

    init

---
+++ analy-toolz/google-api.py
@@ -0,0 +1,19 @@
+"""
+Functions for Google API
+"""
+
+import json
+
+
+def get_auth_type(file):
+    """ Load JSON file and detect Auth type
+    """
+    with open(file, 'r') as f:
+        data = json.load(f)
+    if 'type' in data.keys() and data['type'] == 'service_account':
+        type = 'Service Account'
+    elif 'installed' in data.keys():
+        type = 'OAuth2'
+    else:
+        type = "unknown")
+    return type
