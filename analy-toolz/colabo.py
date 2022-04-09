a873ee71a320e8ea6d09586305c3e3316b3963a9
Makoto Shimizu <mak@fish.razor.jp>
2022-04-09 11:46:16 +0900

    init

---
+++ analy-toolz/colabo.py
@@ -0,0 +1,18 @@
+"""
+Functions for Google Colaboratory
+"""
+
+import os
+from IPython.display import clear_output
+
+from google.colab import drive
+
+
+def mount_gdrive(path="/content/drive"):
+    if not os.path.isdir(path):
+        try:
+            print("Googleドライブがマウントされていないので接続します。")
+            drive.mount(path)
+            clear_output()
+        except Exception as e:
+            raise e
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='example',
+    version='0.0.1',
+    author='Mak Shimizu',
+    author_email='mak@fish.razor.jp',
+    description='Utilities for Google Analytics and Google Cloud Platform.',
+    long_description=long_description,
+    long_description_content_type='ext/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
