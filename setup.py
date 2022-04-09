5cc851df289446e5cf9928ca431f0140ccc37caa
Makoto Shimizu <mak@fish.razor.jp>
2022-04-09 11:55:36 +0900

    bug fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='example',
+    name='analy-toolz',
     version='0.0.1',
     author='Mak Shimizu',
     author_email='mak@fish.razor.jp',
