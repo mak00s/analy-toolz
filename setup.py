e6207e8cdb131981a1273e9b7555b57a4d115fea
Makoto Shimizu <mak@fish.razor.jp>
2022-04-19 19:17:40 +0900

    refactoring

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='analytoolz',
-    version='0.1.2',
+    version='0.1.3',
     author='Mak Shimizu',
     author_email='mak@fish.razor.jp',
     description='Utilities for Google Analytics and Google Cloud Platform.',
