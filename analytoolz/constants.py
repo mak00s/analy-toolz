46fb0e9a0fe867d5c4394616edb9b96c67206c5d
Makoto Shimizu <mak@fish.razor.jp>
2022-04-24 10:06:54 +0900

    add constants

--- analytoolz/constants.py
+++ analytoolz/constants.py
@@ -1,6 +1,7 @@
 """Configurations
 """
 
+# Scopes necessary to use Megaton
 DEFAULT_SCOPES = [
     'https://www.googleapis.com/auth/analytics.readonly',
     'https://www.googleapis.com/auth/analytics.edit',
@@ -8,3 +9,8 @@ DEFAULT_SCOPES = [
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/cloud-platform',
 ]
+
+# URL of Google Sheets template for GA4
+GOOGLE_SHEET_GA4_TEMPLATE_URL = 'https://docs.google.com/spreadsheets/d/1Jr4au61Xy7gfGBf5yvNKdT43awsSv98zOs4FTfM6Nms'
+
+GOOGLE_SHEET_GA4_TEMPLATE_SHEET = '推奨CustomDimension'
